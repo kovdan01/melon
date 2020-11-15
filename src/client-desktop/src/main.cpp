457e244c0ea25da91622e9c05d4a67114e9e5e22
Daniil Kovalev <kovdan01@yandex.ru>
2020-11-15 14:56:00 +0300

    Delete Windows-specific code

--- src/client-desktop/src/main.cpp
+++ src/client-desktop/src/main.cpp
@@ -4,21 +4,6 @@
 
 namespace mc = melon::client_desktop;
 
-#ifdef __MINGW64__
-
-#include <windows.h>
-
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-    int argc = 0;
-    QApplication app(argc, nullptr);
-    mc::MainWindow window;
-    window.show();
-    return QApplication::exec();
-}
-
-#else
-
 int main(int argc, char* argv[])
 {
     QApplication application(argc, argv);
@@ -26,5 +11,3 @@ int main(int argc, char* argv[])
     window.show();
     return QApplication::exec();
 }
-
-#endif
