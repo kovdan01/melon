a1b81fc6c2754b1a030adab79f681ae7d8c5c8fa
h3ll0kitt1 <h3ll0littl3kitt1@gmail.com>
2020-12-04 22:21:44 +0300

    Exchange of sasl mechanisms between server and client

--- src/server/auth/src/main.cpp
+++ src/server/auth/src/main.cpp
@@ -1,67 +1,168 @@
-#include <melon_core.hpp>
 #include <sasl/sasl.h>
 #include <sasl/saslplug.h>
 #include <sasl/saslutil.h>
+
 #include <iostream>
+#include <cstddef>
 #include <cstring>
-#include <unistd.h>
 
-#define N_CALLBACKS (16)
-#define SAMPLE_SEC_BUF_SIZE (2048)
 
-static sasl_callback_t callbacks[N_CALLBACKS];
 
-sasl_conn_t *conn = NULL;
-const char *mech = "PLAIN";
-unsigned len = (unsigned) strlen(mech);
-int count = 1;
-char mechanism_client_chose[SAMPLE_SEC_BUF_SIZE];
+static int ask_for_authID(void *context __attribute__((unused)), int id, const char **result, unsigned *len)
+{
+    static char bufA[1024];
 
+    switch (id)
+    {
+        case SASL_CB_AUTHNAME:
+            printf("please enter an authentication id: ");
+            break;
+        default:
+            return SASL_BADPARAM;
+      }
+
+    fgets(bufA, 1024, stdin);
+    *result = bufA;
+     if (len) *len = strlen(bufA);
+          return SASL_OK;
+}
+
+static sasl_callback_t callbacks[] = {
+  {
+    SASL_CB_GETREALM, NULL, NULL
+  }, {
+    SASL_CB_USER, NULL , NULL
+  }, {
+    SASL_CB_AUTHNAME, (sasl_callback_ft)&ask_for_authID, NULL
+  }, {
+    SASL_CB_PASS, NULL, NULL
+  }, {
+    SASL_CB_LIST_END, NULL, NULL
+  }
+};
+
+
+void snd_str(std::string serverbuf, std::string &clientbuf)
+{
+    clientbuf.assign(serverbuf);
+    //std::cout<<clientbuf<<std::endl;
+}
+
+int sasl_server_new_wrapper(std::string service, sasl_conn_t** context)
+{
+    return sasl_server_new(service.c_str(), nullptr, nullptr, nullptr, nullptr, nullptr, 0, context);
+}
+
+int sasl_client_new_wrapper(std::string service, sasl_conn_t** context)
+{
+    return sasl_client_new(service.c_str(), nullptr, nullptr, nullptr, nullptr, 0, context);
+}
 
 int main()
 {
-    int result;
 
-    std::cout<<"Use default configure file sample.conf "<<std::endl;
-    result = sasl_server_init(callbacks, "sample");
-    if (result!=SASL_OK)
+    std::string mechclient;
+    std::string mechserver;
+
+    mechclient = "PLAIN"; //chosen by client - we should ask to enter this
+
+    int result = sasl_server_init(nullptr, "sample");
+    if (result != SASL_OK)
     {
-        std::cout << "Fail server_init"<<std::endl;
+        std::cout<<"Fail sasl_server_init()"<<std::endl;
     }
 
-    //someservice?
-     std::cout<<"Init server"<<std::endl;
-     result = sasl_server_new("someservice", /*"localhost.localdomain."*/ NULL, NULL, NULL, NULL, NULL, 0, &conn);
-     if (result != SASL_OK)
+    //server context
+    sasl_conn_t *connserver;
+
+    result = sasl_server_new_wrapper("fake", &connserver);
+    if (result != SASL_OK)
+    {
+        std::cout<<"Fail sasl_server_new()"<<std::endl;
+    }
+
+    //in callbacks defined that future authID will be asked
+    result = sasl_client_init(callbacks);
+    if (result != SASL_OK)
+    {
+        std::cout<<"Fail client_init"<<std::endl;
+    }
+
+    //client context
+    sasl_conn_t *connclient;
+
+    result = sasl_client_new_wrapper("fake", &connclient);
+    if (result != SASL_OK)
+    {
+        std::cout<<"Fail client_new"<<std::endl;
+    }
+
+    //currently chosen mech by client - no mech yet
+    std::string clientbuf = "NO MECH NOW";
+
+    //negotiation server to client: server prints which mechanisms are available
+    printf("generating available  sasl mechanisms... \n");
+    const char* data;
+    int len;
+    int count;
+
+    result  = sasl_listmech(connserver, nullptr, nullptr, " ", nullptr, &data, (unsigned int *) &len, &count);
+    if (result  != SASL_OK)
+            std::cout<<"Fail generating mechanism list"<<std::endl;
+    std::cout<<"Number of mechanisms: " << count <<std::endl;
+    std::cout<<"Mechanisms: "<<data<<std::endl;
+    snd_str(data, mechserver);
+
+    //server send mechs to client
+    std::cout<<"Send possible mechanisms to client"<<std::endl;
+    snd_str(data, clientbuf);
+
+    //client recieve mechs from server
+    std::cout<<clientbuf<<std::endl;
+
+
+    //client checks that mech, that it wants, is in the list
+    if (clientbuf.find(mechclient) != std::string::npos)
      {
-         std::cout <<"Fail server_new"<<std::endl;
+           std::cout<<mechclient<<" is found, so can be used "<<std::endl;
      }
 
-     std::cout<<"Generating client mech list"<<std::endl;
-     result = sasl_listmech(conn, NULL, "{", ", ", "}", &mech, &len, &count);
-     if (result != SASL_OK)
-     {
-         std::cout <<"Fail mech"<<std::endl;
-     }
-     std::cout<<"Look what client chose as mech"<<std::endl;
+     //client do: sasl_client_start
+      const char *dataclient;
+      const char *chosenmechclient;
+      int lenclient;
 
-    //why you are not waiting... please sleep?
+      //client should start authatification depending on its callback, but smth fails and there are problems with memory
+      result = sasl_client_start(connclient, mechclient.c_str(), nullptr, &dataclient, (unsigned int *) &lenclient, &chosenmechclient);
+      if(result == SASL_OK)
+      {
+            std::cout<<"SASL_OK"<<std::endl;
+      }
+
+        //result=sasl_client_step(connclient, in,   inlen, &client_interact,  &out, &outlen); /* filled in on success */
 
 
-    const char *out;
-    unsigned outlen;
+        //result  = sasl_server_start(connserver, mechclient.c_str(), buf, len, &data, (unsigned int *) &len);
 
-    result = sasl_server_start(conn,  mechanism_client_chose, NULL,  0, &out, &outlen);
+//        result = sasl_server_step(connserver, buf, len, &data, (unsigned int *) &len);
 
-    if ((result!=SASL_OK) && (result!=SASL_CONTINUE))
-    {
-        std::cout<<"fail to give client's mech"<<std::endl;
-    }
-    else if (result==SASL_OK)
-    {
-        std::cout<<"success"<<std::endl;
-    }
+//        result = sasl_getprop(connserver, SASL_USERNAME, (const void **) &userid);
 
-     sasl_dispose(&conn);
+        if(result == SASL_CONTINUE)
+        {
+             std::cout<<"wainting for some steps"<<std::endl;
+        }
+
+        if (result != SASL_OK && result != SASL_CONTINUE) {
+                std::cout<<"error"<<std::endl;
+            }
+
+        std::cout<<"done"<<std::endl;
+
+
+        sasl_dispose(&connserver);
+        sasl_dispose(&connclient);
+        sasl_client_done();
+
 
 }
