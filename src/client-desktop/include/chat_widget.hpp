e5e0c1c55cf26ee7c76410ef318296de535bef7e
Gavarina Svetlana <56734508+Ctrl-Admiral@users.noreply.github.com>
2020-12-07 23:06:57 +0300

    Add draft desktop client (#24)
    Features:
    
    * Chat list on the left panel
    * Chat on the right panel
    * Chat adding, deleting and renaming
    
    Known bugs: #61

---
+++ src/client-desktop/include/chat_widget.hpp
@@ -0,0 +1,44 @@
+#ifndef MELON_CLIENT_DESKTOP_CHAT_WIDGET_HPP_
+#define MELON_CLIENT_DESKTOP_CHAT_WIDGET_HPP_
+
+#include "ui_chat_widget.h"
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui  // NOLINT (readability-identifier-naming)
+{
+class ChatWidget;
+}  // namespace Ui
+QT_END_NAMESPACE
+
+namespace melon::client_desktop
+{
+
+class ChatWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    ChatWidget(QWidget* parent = nullptr);
+    ~ChatWidget() override = default;
+
+private slots:
+    void send_message();
+    void receive_message();
+
+private:  // NOLINT (readability-redundant-access-specifiers)
+    QScopedPointer<Ui::ChatWidget> m_ui;
+};
+
+namespace rgba_receive
+{
+constexpr int R = 250;
+constexpr int G = 224;
+constexpr int B = 180;
+constexpr int A = 127;
+}  // namespace rgba_receive
+
+}  // namespace melon::client_desktop
+
+#endif  // MELON_CLIENT_DESKTOP_CHAT_WIDGET_HPP_
