90cdfff529a44546fda4e5ebacaf16a1c3461ab6
Daniil Kovalev <kovdan01@yandex.ru>
2021-01-25 23:31:18 +0300

    Implement basic one-step authentication

---
+++ src/server/auth/include/sasl_wrapper.hpp
@@ -0,0 +1,240 @@
+#ifndef MELON_SERVER_AUTH_SASL_WRAPPER_HPP_
+#define MELON_SERVER_AUTH_SASL_WRAPPER_HPP_
+
+#include <melon/server/auth/export.h>
+
+#include <sasl/saslutil.h>
+#include <sasl/sasl.h>
+#include <sasl/saslplug.h>
+
+#include <array>
+#include <cstdlib>
+#include <cstring>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <utility>
+
+namespace melon::server::auth
+{
+
+using sasl_res = int;
+
+struct SASL_WRAPPERS_EXPORT Credentials
+{
+    const std::string username;
+    const std::string password;
+};
+
+namespace detail
+{
+
+inline void check_sasl_result(sasl_res res, std::string_view function_name)
+{
+    if (res != SASL_OK && res != SASL_CONTINUE)
+    {
+        throw std::runtime_error("Sasl " + std::string(function_name) + " exit code " +
+                                 std::to_string(res) + ": " + sasl_errstring(res, nullptr, nullptr));
+    }
+}
+
+namespace callbacks
+{
+
+inline sasl_res get_username(void* context, int id, const char** result, unsigned* len)
+{
+    auto* params = static_cast<Credentials*>(context);
+    switch (id)
+    {
+    case SASL_CB_USER:
+    case SASL_CB_AUTHNAME:
+        // std::cerr << "Setting username to " << params->username << std::endl;
+        *result = params->username.c_str();
+        if (len != nullptr)
+            *len = static_cast<unsigned>(params->username.size());
+        break;
+    default:
+        return SASL_BADPARAM;
+    }
+    return SASL_OK;
+}
+
+inline sasl_res get_password(sasl_conn_t*, void* context, int id, sasl_secret_t** psecret)
+{
+    if (id != SASL_CB_PASS)
+        return SASL_BADPARAM;
+    auto* params = static_cast<Credentials*>(context);
+    // std::cerr << "Setting pass to " << params->password << std::endl;
+
+    static sasl_secret_t* secret;
+    auto* temp = reinterpret_cast<sasl_secret_t*>(std::realloc(secret, sizeof(sasl_secret_t) + params->password.size()));
+    if (temp != nullptr)
+        secret = temp;
+    else
+        return SASL_NOMEM;
+    secret->len = params->password.size();
+    std::memcpy(secret->data, params->password.c_str(), secret->len + 1);
+    *psecret = secret;
+
+    return SASL_OK;
+}
+
+}  // namespace callbacks
+
+}  // namespace detail
+
+class SASL_WRAPPERS_EXPORT SaslServer
+{
+public:
+    SaslServer(std::string service)
+        : m_service(std::move(service))
+    {
+        sasl_res res = sasl_server_new(m_service.c_str(), nullptr, nullptr, nullptr, nullptr, nullptr, 0, &m_conn);
+        detail::check_sasl_result(res, "server new");
+    }
+
+    ~SaslServer()
+    {
+        sasl_dispose(&m_conn);
+    }
+
+    std::string_view list_mechanisms()
+    {
+        const char* data;
+        unsigned plen;
+        int pcount;
+        sasl_res res = sasl_listmech(m_conn, nullptr, nullptr, " ", nullptr, &data, &plen, &pcount);
+
+        detail::check_sasl_result(res, "listmech");
+
+        return { data, plen };
+    }
+
+    std::string_view start(std::string_view chosen_mechanism, std::string_view client_initial_response)
+    {
+        const char* serverout;
+        unsigned serverout_len;
+        sasl_res res = sasl_server_start(m_conn, chosen_mechanism.data(), client_initial_response.data(),
+                                         static_cast<unsigned>(client_initial_response.size()), &serverout, &serverout_len);
+
+        detail::check_sasl_result(res, "server start");
+
+        return { serverout, serverout_len };
+    }
+
+    [[nodiscard]] const sasl_conn_t* conn() const
+    {
+        return m_conn;
+    }
+
+    [[nodiscard]] sasl_conn_t* conn()
+    {
+        return m_conn;
+    }
+
+private:
+    const std::string m_service;
+    sasl_conn_t* m_conn;
+};
+
+class SASL_WRAPPERS_EXPORT SaslServerSingleton
+{
+public:
+    SaslServerSingleton()
+    {
+        sasl_res res = sasl_server_init(nullptr, "localserver");
+        detail::check_sasl_result(res, "server init");
+    }
+
+    ~SaslServerSingleton()
+    {
+        sasl_server_done();
+    }
+
+private:
+    static SaslServerSingleton* g_instance;
+};
+
+class SASL_WRAPPERS_EXPORT SaslClient
+{
+public:
+    SaslClient(std::string service)
+        : m_service(std::move(service))
+    {
+        sasl_res res = sasl_client_new(m_service.c_str(), nullptr, nullptr, nullptr, nullptr, 0, &m_conn);
+        detail::check_sasl_result(res, "client new");
+    }
+
+    ~SaslClient()
+    {
+        sasl_dispose(&m_conn);
+    }
+
+    struct StartResult
+    {
+        std::string_view response;
+        std::string_view selected_mechanism;
+    };
+
+    StartResult start(std::string_view wanted_mech_list)
+    {
+        const char* out;
+        const char* selected_mechanism;
+        unsigned len;
+        sasl_res res = sasl_client_start(m_conn, wanted_mech_list.data(), nullptr, &out, &len, &selected_mechanism);
+        detail::check_sasl_result(res, "client start");
+
+        return StartResult{ .response = { out, len }, .selected_mechanism = selected_mechanism };
+    }
+
+    [[nodiscard]] const sasl_conn_t* conn() const
+    {
+        return m_conn;
+    }
+
+    [[nodiscard]] sasl_conn_t* conn()
+    {
+        return m_conn;
+    }
+
+private:
+    const std::string m_service;
+    sasl_conn_t* m_conn;
+};
+
+class SASL_WRAPPERS_EXPORT SaslCientSingleton
+{
+public:
+    SaslCientSingleton()
+        : m_callbacks
+          {
+              sasl_callback_t{ .id = SASL_CB_USER,     .proc = reinterpret_cast<sasl_callback_ft>(&detail::callbacks::get_username), .context = nullptr },
+              sasl_callback_t{ .id = SASL_CB_AUTHNAME, .proc = reinterpret_cast<sasl_callback_ft>(&detail::callbacks::get_username), .context = nullptr },
+              sasl_callback_t{ .id = SASL_CB_PASS,     .proc = reinterpret_cast<sasl_callback_ft>(&detail::callbacks::get_password), .context = nullptr },
+              sasl_callback_t{ .id = SASL_CB_LIST_END, .proc = nullptr,                                                              .context = nullptr }
+          }
+    {
+        sasl_res res = sasl_client_init(m_callbacks.data());
+        detail::check_sasl_result(res, "client init");
+    }
+
+    void set_credentials(Credentials* credentials)
+    {
+        m_callbacks[0].context = credentials;
+        m_callbacks[1].context = credentials;
+        m_callbacks[2].context = credentials;
+    }
+
+    ~SaslCientSingleton()
+    {
+        sasl_server_done();
+    }
+
+private:
+    static SaslServerSingleton* g_instance;
+    std::array<sasl_callback_t, 4> m_callbacks;
+};
+
+}  // namespace melon::server::auth
+
+#endif  // MELON_SERVER_AUTH_SASL_WRAPPER_HPP_
--- src/server/auth/src/main.cpp
+++ src/server/auth/src/main.cpp
@@ -1,210 +1,34 @@
-#include <melon/core.hpp>
+#include <sasl_wrapper.hpp>
 
-#include <sasl/saslutil.h>
-#include <sasl/sasl.h>
-#include <sasl/saslplug.h>
-
-#include <algorithm>
-#include <array>
-#include <cstring>
+#include <cassert>
 #include <iostream>
 
-constexpr std::uint64_t MAX_MESSAGE_LEN = 25600;
-const std::string wanted_mech = "PLAIN";
-
-using saslret = int;
-
-namespace fakenet  // just what it says on the tin: this is a simulation of network data channel
+int main() try
 {
-    using byte_t = unsigned char;
+    namespace msa = melon::server::auth;
 
-    static std::array<byte_t, MAX_MESSAGE_LEN> network;
+    static const msa::SaslServerSingleton server_singletone;
 
-    template <typename T>
-    T read_type(const byte_t* from = network.data())  // yep
-    {
-        T ans = 0;
-        for (std::size_t shift = 0; shift != sizeof(T) * 8; shift += 8, ++from)
-            ans |= static_cast<T>(*from) << shift;
-        return ans;
-    }
+    msa::SaslCientSingleton client_singletone;
+    msa::Credentials credentials = { .username = "cat", .password = "dog" };
+    client_singletone.set_credentials(&credentials);
 
-    template <typename T>
-    void write_type(T what, byte_t* where = network.data())
-    {
-        T mask = 0xff;
-        for (std::size_t shift = 0; shift != sizeof(T) * 8; mask <<= 8, ++where, shift += 8)
-            *where = (what & mask) >> shift;
-    }
+    msa::SaslServer server("melon");
+    std::string_view supported_mechanisms = server.list_mechanisms();
 
-    static void write_bytes(byte_t* from, std::size_t len)
-    {
-        std::memmove(network.data(), from, len);
-    }
+    msa::SaslClient client("melon");
+    const std::string wanted_mechanism = "PLAIN";
+    if (supported_mechanisms.find(wanted_mechanism) == std::string_view::npos)
+        throw std::runtime_error("Wanted machanism " + wanted_mechanism + " is not supported by server. Supported mechanisms: " + std::string(supported_mechanisms));
 
-    static byte_t* read_bytes(byte_t* where, std::size_t len)
-    {
-        std::memmove(where, network.data(), len);
-        return where;
-    }
+    auto [client_response, selected_mechanism] = client.start(wanted_mechanism);
+    assert(selected_mechanism == wanted_mechanism);
 
-
-}  // namespace callbacks
-
-namespace callbacks
-{
-    struct MyContext
-    {
-        const std::string username;
-        const std::string password;
-    };
-
-    static saslret login_name(void* context, int id, const char** result, unsigned* len)
-    {
-        auto* params = static_cast<MyContext*>(context);
-        switch (id)
-        {
-        case SASL_CB_USER:
-        case SASL_CB_AUTHNAME:
-            std::cout << "Setting username to " << params->username <<std::endl;
-            *result = params->username.c_str();
-            if(len != nullptr)
-                *len = static_cast<unsigned>(params->username.size());
-            break;
-        default:
-            return SASL_BADPARAM;
-        }
-        return SASL_OK;
-    }
-
-    static saslret login_pass(sasl_conn_t*, void* context, int id, sasl_secret_t** psecret)
-    {
-        if(id != SASL_CB_PASS)
-            return SASL_BADPARAM;
-        auto* params = static_cast<MyContext*>(context);
-        std::cout << "Setting pass to " << params->password <<std::endl;
-        // somehow rewrite this proper C++ way
-        static sasl_secret_t* secret;
-        auto* temp = reinterpret_cast<sasl_secret_t*>(std::realloc(secret, sizeof(sasl_secret_t) + params->password.size()));
-        if (temp != nullptr)
-            secret = temp;
-        else
-            return SASL_NOMEM;
-        secret->len = params->password.size();
-        std::memcpy(secret->data, params->password.c_str(), secret->len + 1);
-        *psecret = secret;
-        // somehow rewrite this proper C++ way
-
-        return SASL_OK;
-    }
-
-
-
-} // namespace fakenet
-
-int main()
-{
-    melon::core::hello();
-
-    // cyrus-SASL server
-    saslret retcode = sasl_server_init(nullptr, "localserver");
-    if(retcode != SASL_OK)
-    {
-      throw std::runtime_error("server inititalization: " + std::string(sasl_errstring(retcode, nullptr, nullptr)));
-    }
-
-    // cyrus-SASL client
-    callbacks::MyContext context = {"kyle","kp"};
-    static std::array<sasl_callback_t, 4> callbacks = {{
-        {SASL_CB_USER, reinterpret_cast<sasl_callback_ft>(&callbacks::login_name), &context},
-        {SASL_CB_AUTHNAME, reinterpret_cast<sasl_callback_ft>(&callbacks::login_name), &context},
-        {SASL_CB_PASS, reinterpret_cast<sasl_callback_ft>(&callbacks::login_pass), &context},
-        {SASL_CB_LIST_END, nullptr, nullptr}
-      }};
-    retcode = sasl_client_init(callbacks.data());
-    if(retcode != SASL_OK)
-    {
-      throw std::runtime_error("client inititalization: " + std::string(sasl_errstring(retcode, nullptr, nullptr)));
-    }
-
-    /*
-     * connection is set up through fakenet
-     */
-
-    // cyrus-SASL server
-    sasl_conn_t* server_conn;
-    retcode = sasl_server_new("conn", nullptr, nullptr, nullptr, nullptr, nullptr, 0, &server_conn);
-    if(retcode != SASL_OK)
-    {
-      throw std::runtime_error("server new connection: " + std::string(sasl_errstring(retcode, nullptr, nullptr)));
-    }
-    const char* server_capabilites_ptr;
-    unsigned int server_capabilites_len;
-    retcode = sasl_listmech(server_conn, nullptr, nullptr, " ", nullptr, &server_capabilites_ptr, &server_capabilites_len, nullptr);
-    if(retcode != SASL_OK)
-    {
-      throw std::runtime_error("server mech capabilities: " + std::string(sasl_errstring(retcode, nullptr, nullptr)));
-    }
-    std::string server_servermechs(server_capabilites_ptr, server_capabilites_len);
-    fakenet::write_type<std::size_t>(server_servermechs.size());
-
-    // cyrus-SASL client
-    sasl_conn_t* client_conn;
-    retcode = sasl_client_new("conn", nullptr, nullptr, nullptr, nullptr, 0, &client_conn);
-    if(retcode != SASL_OK)
-    {
-      throw std::runtime_error("client new connection: " + std::string(sasl_errstring(retcode, nullptr, nullptr)));
-    }
-    std::size_t client_servermechs_len = fakenet::read_type<std::size_t>();
-
-    // cyrus-SASL server
-    fakenet::write_bytes(reinterpret_cast<unsigned char*>(server_servermechs.data()),server_servermechs.size());
-
-    // cyrus-SASL client
-    std::array<char, MAX_MESSAGE_LEN> client_recieved_buffer;
-    fakenet::read_bytes(reinterpret_cast<unsigned char*>(client_recieved_buffer.data()),client_servermechs_len);
-    client_recieved_buffer[client_servermechs_len]='\0';
-
-    /*
-     * client checks if he can use the wanted mech, sends it to server, server confirms that he's ready
-     */
-
-    // cyrus-SASL client
-    const char* client_saslout;
-    const char* client_saslselectedmech;
-    unsigned int client_saslout_len;
-    retcode = sasl_client_start(client_conn, /*client_recieved_buffer.data()*/wanted_mech.c_str(), nullptr, &client_saslout, &client_saslout_len, &client_saslselectedmech);
-    if(retcode != SASL_OK && retcode != SASL_CONTINUE)
-    {
-      throw std::runtime_error("client negotioation start: " + std::string(sasl_errstring(retcode, nullptr, nullptr)));
-    }
-    std::cerr<<std::string(client_saslout, client_saslout_len)<<"\n";
-    fakenet::write_bytes(reinterpret_cast<unsigned char*>(const_cast<char*>(client_saslout)), client_saslout_len);
-    // cyrus-SASL server
-    std::array<char, MAX_MESSAGE_LEN> server_recieved_buffer;
-    fakenet::read_bytes(reinterpret_cast<unsigned char*>(server_recieved_buffer.data()), client_saslout_len); // !! client_saslout_len
-    //server_recieved_buffer[client_saslout_len]='\0'; // !!
-    const char* server_saslout;
-    unsigned int server_saslout_len;
-    retcode = sasl_server_start(server_conn, wanted_mech.c_str(), server_recieved_buffer.data(), client_saslout_len, &server_saslout, &server_saslout_len);
-    if(retcode != SASL_OK && retcode != SASL_CONTINUE)
-    {
-      throw std::runtime_error("server negotioation start: " + std::string(sasl_errstring(retcode, nullptr, nullptr)));
-    }
-    std::cerr << retcode <<'\n';
-    /*iretcode = sasl_server_step(server_conn, server_recieved_buffer.data(), client_saslout_len, &server_saslout, &server_saslout_len);
-    if (retcode != SASL_OK && retcode != SASL_CONTINUE)
-    {
-      throw std::runtime_error("server negotioation contin: " + std::string(sasl_errstring(retcode, nullptr, nullptr)));
-    }*/
-    std::cerr << "Memory cleanup\n";
-    // cyrus-SASL client
-    sasl_dispose(&client_conn);
-    sasl_client_done();
-
-    // cyrus-SASL server
-    sasl_dispose(&server_conn);
-    sasl_server_done();
+    [[maybe_unused]] std::string_view server_response = server.start(selected_mechanism, client_response);
 
     return 0;
+}
+catch (const std::exception& e)
+{
+    std::cerr << e.what() << '\n';
 }
