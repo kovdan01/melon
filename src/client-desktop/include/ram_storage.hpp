40a2c3e91f17f5bce8021fab4e2679b3779eb985
Daniil Kovalev <kovdan01@yandex.ru>
2021-02-07 18:51:05 +0300

    Add RAM storage files

---
+++ src/client-desktop/include/ram_storage.hpp
@@ -0,0 +1,11 @@
+#ifndef MELON_CLIENT_DESKTOP_RAM_STORAGE_HPP_
+#define MELON_CLIENT_DESKTOP_RAM_STORAGE_HPP_
+
+namespace melon::client_desktop
+{
+
+
+
+}  // namespace melon::client_desktop
+
+#endif // MELON_CLIENT_DESKTOP_RAM_STORAGE_HPP_
---
+++ src/client-desktop/src/ram_storage.cpp
@@ -0,0 +1,8 @@
+#include <ram_storage.hpp>
+
+namespace melon::client_desktop
+{
+
+
+
+}  // namespace melon::client_desktop
