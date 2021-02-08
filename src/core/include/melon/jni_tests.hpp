9007acba8ede4c5c7c1dea35fe3eccca5793402b
Roman Anchugov <r.anchugov@rambler-co.ru>
2021-02-08 14:47:05 +0300

    Create temp tests for JNI

---
+++ src/core/include/melon/jni_tests.hpp
@@ -0,0 +1,19 @@
+//
+// Created by r.anchugov on 08.02.2021.
+//
+
+#ifndef MELON_CORE_JNI_TESTS_HPP
+#define MELON_CORE_JNI_TESTS_HPP
+
+int int_test_sum(int a, int b);
+
+double double_test_sum(double a, double b);
+
+std::uint64_t std_test_sum(std::uint64_t a, std::uint64_t b);
+
+std::string string_test();
+
+const char* buff_test();
+
+
+#endif //MELON_CORE_JNI_TESTS_HPP
