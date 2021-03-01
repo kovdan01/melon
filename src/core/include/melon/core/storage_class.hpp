7eecebd54ad14f709b51d1efb0e8ebf62856ec25
Daniil Kovalev <kovdan01@yandex.ru>
2021-03-02 01:49:25 +0300

    Suppress false-positive clang-tidy warnings

--- src/core/include/melon/core/storage_class.hpp
+++ src/core/include/melon/core/storage_class.hpp
@@ -13,7 +13,7 @@ namespace melon::core
 class MELON_CORE_EXPORT User
 {
 public:
-    enum class Status : unsigned long  // NOLINT google-runtime-int
+    enum class Status : unsigned long  // NOLINT (google-runtime-int)
     {
         OFFLINE,
         ONLINE,
@@ -36,7 +36,7 @@ private:
 class MELON_CORE_EXPORT Message
 {
 public:
-    enum class Status : unsigned long  // NOLINT google-runtime-int
+    enum class Status : unsigned long  // NOLINT (google-runtime-int)
     {
         FAIL,
         SENT,
