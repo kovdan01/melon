5a1228cdf7dee31df1eeed3fc88b5a34bf4730b4
h3ll0kitt1 <52863098+h3ll0kitt1@users.noreply.github.com>
2021-03-01 23:22:08 +0300

    Add mariadb support (#74)

---
+++ src/core/src/storage_class.cpp
@@ -0,0 +1,32 @@
+#include <melon/storage_class.hpp>
+
+#include <iostream>
+
+namespace melon::core
+{
+
+User::User(std::uint64_t user_id, std::string username, Status status)
+    : m_user_id(user_id)
+    , m_username(std::move(username))
+    , m_status(status)
+{
+}
+
+Message::Message(std::uint64_t message_id, std::uint64_t user_id,
+                 std::uint64_t chat_id, std::string text, Message::Status status)
+    : m_message_id(message_id)
+    , m_user_id(user_id)
+    , m_chat_id(chat_id)
+    , m_text(std::move(text))
+    , m_status(status)
+{
+}
+
+Chat::Chat(std::uint64_t chat_id, std::string chatname)
+    : m_chat_id(chat_id)
+    , m_chatname(std::move(chatname))
+{
+}
+
+
+}  // namespace melon::core
--- src/server/storage/src/main.cpp
+++ src/server/storage/src/main.cpp
@@ -1,134 +1,69 @@
-#include <melon/core.hpp>
-
-#include <yaml-cpp/yaml.h>
-
-#include <array>
-#include <exception>
 #include <iostream>
-#include <string>
-#include <vector>
 
-namespace meco = melon::core;
+#include <storage.hpp>
 
-static void parameter_abnormalities_reaction(const std::vector<std::string>& missing_params, const std::vector<std::string>& superfluous_params, const std::string& where)
+
+int main() try
 {
-    if (!missing_params.empty())
-        throw meco::yaml_conf::MissingParamsException("missing required params at " + where);
-    if (!superfluous_params.empty())
-        throw meco::yaml_conf::ExtraParamsException("extra params at " + where);
+
+    namespace mss = melon::server::storage;
+    namespace mc = melon::core;
+    namespace mysql = sqlpp::mysql;
+
+
+    mysql::connection db(mss::config_melondb());
+
+
+
+    mc::User user(0, "h3ll0kitt1", mc::User::Status::ONLINE);
+    mss::add_user(db, user);
+
+    mc::Chat chat(0,"secret_chat");
+    mss::add_chat(db, chat);
+
+    mc::Message message(0, 1, 1, "Let's protest", mc::Message::Status::SENT);
+    mss::add_message(db, message);
+
+    mc::Message message2(0, 1, 1, "or go to OVD", mc::Message::Status::RECEIVED);
+    mss::add_message(db, message2);
+
+    std::vector<mc::Message> chat_message = mss::get_messages_for_chat(db, chat);
+    std::cout << "Messages of chat: " << std::endl;
+    for (const auto& a: chat_message)
+    {
+        std:: cout << "text: " << a.text() << std::endl;
+    }
+
+    std::vector<mc::User> online_users = mss::get_online_users(db);
+    std::cout << "Online users: " << std::endl;
+    for (auto& a: online_users)
+    {
+        std:: cout << a.user_id() << " : " << a.username() << std::endl;
+    }
+
+    std::cout << "Change status for online" << std::endl;
+    mss::make_user_online(db, user);
+    std::vector<std::string> online_users_names = mss::get_online_users_names(db);
+
+    std::cout << "Online users: " << std::endl;
+    for (const auto &a: online_users_names)
+    {
+        std:: cout << a << std::endl;
+    }
+
+    std::cout << "Change status for offline" << std::endl;
+    mss::make_user_offline(db, user);
+    online_users_names = mss::get_online_users_names(db);
+
+    std::cout << "Online users: " << std::endl;
+    for (auto& a: online_users_names)
+    {
+        std:: cout << a << std::endl;
+    }
+
 }
-
-template <std::size_t Size>
-static void parse_locations(const std::string& title, const YAML::Node& node, const std::array<std::string, Size>& required_parameters_locations)
+catch (const sqlpp::exception& e)
 {
-    for (const YAML::Node& node_of_list : node)
-    {
-        auto [parsed, abnormal] = meco::yaml_conf::parse_one_level_down(node_of_list,
-                                                                        required_parameters_locations.begin(),
-                                                                        required_parameters_locations.end());
-        auto [missing_params, superfluous_params] = abnormal;
-        parameter_abnormalities_reaction(missing_params, superfluous_params, title);
-        std::string kind, ip;
-        for (auto& [title, node2] : parsed)
-        {
-            if (title == "service_kind")
-                kind = node2.template as<std::string>();
-            else if (title == "ip")
-                ip = node2.template as<std::string>();
-
-            // else if (title == "...")
-            // ...
-
-        }
-        std::cout << "The " << kind << " service is at " << ip << '\n';
-    }
-}
-
-template <std::size_t Size>
-static void parse_networking(const std::string& title, const YAML::Node& node, const std::array<std::string, Size>& required_parameters_networking)
-{
-    auto [parsed, abnormal] = meco::yaml_conf::parse_one_level_down(node,
-                                                                required_parameters_networking.begin(),
-                                                                required_parameters_networking.end());
-    auto& [missing_params, superfluous_params] = abnormal;
-    parameter_abnormalities_reaction(missing_params, superfluous_params, title);
-    for (auto& [title, node2] : parsed)
-    {
-        if (title == "ip")
-        {
-            auto ip = node2.template as<std::string>();
-            std::cout << "I am at " << ip << '\n';
-        }
-
-        // else if ( title == "...")
-        // ...
-    }
-}
-
-
-static void auth_config_parse(const YAML::Node& conf_file)
-{
-
-    // Parameters we want on top level of YAML
-    static const std::array<std::string, 3> required_parameters =
-    {
-        "networking",
-        "locations",
-        "db-location",
-    };
-    // Networking parameters we want
-    static const std::array<std::string, 3> required_parameters_networking =
-    {
-        "ip",
-        "port",
-        "protocol",
-    };
-    // Other service locations parameters we want
-    static const std::array<std::string, 4> required_parameters_locations =
-    {
-        "service_kind",
-        "ip",
-        "port",
-        "protocol",
-    };
-
-    //  Parse the top level of YAML and notify about parameter abnormalities
-    auto parsed_level_1 = meco::yaml_conf::parse_one_level_down(conf_file, required_parameters.begin(), required_parameters.end());
-    auto [missing_params, superfluous_params] = parsed_level_1.second;
-    parameter_abnormalities_reaction(missing_params, superfluous_params, "top level");
-    // Go deeper
-    for (auto& [title, node] : parsed_level_1.first)
-    {
-        if (title == "networking")
-            parse_networking(title, node, required_parameters_networking);
-        else if (title == "locations")
-            parse_locations(title, node, required_parameters_locations);
-        else if (title == "db-location")
-            std::cout << "Database is located at " << node.as<std::string>() << '\n';
-        else
-            throw std::runtime_error("Parsing logic failure");  // Something went really wrong, let's throw an exception...
-    }
-}
-
-
-int main()
-{
-    // Parameter parsing example
-
-    // Note that this is an example:
-    // - all required parameters are hardcoded
-    // - nothing is returned from function
-    try
-    {
-        YAML::Node conf_file = YAML::LoadFile("m_config.yaml");
-        auth_config_parse(conf_file);
-    }
-    catch (const std::exception& e)
-    {
-        std::cerr << "Configuration file parsing exception: " << e.what() << '\n';
-        // ...
-    }
-
-    meco::hello();
-    return 0;
+    std::cerr << "For testing, you'll need to create a database melon for user 'melon' on localhost with password 'melonpass'" << std::endl;
+    std::cerr << e.what() << std::endl;
 }
