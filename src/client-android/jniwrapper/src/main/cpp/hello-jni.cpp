9d9f9415f3ed87b510187ed7e7a357170b10ca3b
Daniil Kovalev <kovdan01@yandex.ru>
2021-03-03 22:02:35 +0300

    Fix build

--- src/client-android/jniwrapper/src/main/cpp/hello-jni.cpp
+++ src/client-android/jniwrapper/src/main/cpp/hello-jni.cpp
@@ -5,6 +5,6 @@
 extern "C" JNIEXPORT jint JNICALL
 Java_org_melon_jniwrapper_TestWrapper_another(JNIEnv *env, jobject thiz)
 {
-    std::string str = string_test();
+    std::string str = melon::core::string_test();
     return str.length();
 }
