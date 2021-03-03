d565e675e5bf5eb107bbcf78201fe27ddf316933
Roman Anchugov <r.anchugov@rambler-co.ru>
2021-03-03 19:43:50 +0300

    Jni usage

--- src/client-android/app/src/main/java/org/melon/melonmessenger/presentation/MainActivity.kt
+++ src/client-android/app/src/main/java/org/melon/melonmessenger/presentation/MainActivity.kt
@@ -1,10 +1,25 @@
 package org.melon.melonmessenger.presentation
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import org.melon.jniwrapper.TestWrapper
 import org.melon.melonmessenger.R
 import org.melon.melonmessenger.presentation.base.BaseActivity
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity(R.layout.activity_main) {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        testTv.text = TestWrapper().another().toString()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.tag("LOL").i("RESULTS IS: ${TestWrapper().another()}")
+    }
 }
--- src/client-android/jniwrapper/src/main/cpp/hello-jni.cpp
+++ src/client-android/jniwrapper/src/main/cpp/hello-jni.cpp
@@ -3,10 +3,8 @@
 #include <iostream>
 #include <melon/jni_tests.hpp>
 
-
 extern "C" JNIEXPORT jint JNICALL
 Java_org_melon_jniwrapper_TestWrapper_another(JNIEnv *env, jobject thiz) {
     auto str = string_test();
-
     return str.length();
 }
