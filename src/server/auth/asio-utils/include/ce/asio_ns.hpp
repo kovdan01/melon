6330ea03383105c96628b574f4a45e12ae157799
tyyu3 <tyyu@list.ru>
2021-03-08 14:33:20 +0300

    copy server base from cosec-examples

---
+++ src/server/auth/asio-utils/include/ce/asio_ns.hpp
@@ -0,0 +1,14 @@
+#ifndef UUID_31854F41_7280_4A0B_9917_9069600D19C3
+#define UUID_31854F41_7280_4A0B_9917_9069600D19C3
+
+namespace boost::asio::execution {}
+namespace boost::beast {}
+
+namespace ce
+{
+    namespace ba = boost::asio;
+    namespace bae = boost::asio::execution;
+    namespace bb = boost::beast;
+}
+
+#endif
