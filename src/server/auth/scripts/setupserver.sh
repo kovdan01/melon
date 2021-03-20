eaf9e7946a835e9f9446a30a151b39ae9bc2e939
tyyu3 <tyyu@list.ru>
2021-03-21 00:57:02 +0300

    Script change

--- src/server/auth/scripts/setupserver.sh
+++ src/server/auth/scripts/setupserver.sh
@@ -4,7 +4,7 @@ path_to_configs=$HOME/.melon
 current_path=`pwd`
 cd $path_to_configs
 touch sasldb2
-echo "doe" | saslpasswd2 -f sasldb2 -c john
+echo "doe" | saslpasswd2 -f $path_to_configs/sasldb2 -c john
 echo -n "pwcheck_method: auxprop
 sasldb_path: " > localserver.conf
 echo -n $path_to_configs/sasldb2 >> localserver.conf
