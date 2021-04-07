059e1af16db9317214a6233b9ea6f64ea1eb279f
Daniil Kovalev <kovdan01@yandex.ru>
2021-04-07 10:43:35 +0300

    Break tests

--- src/server/storage/src/storage_tests.cpp
+++ src/server/storage/src/storage_tests.cpp
@@ -1,5 +1,7 @@
 #include <catch2/catch.hpp>
 
+#include <algorithm>
+#include <iostream>
 #include <tuple>
 
 #include <storage.hpp>
@@ -8,42 +10,72 @@ namespace mc = melon::core;
 namespace mss = melon::server::storage;
 namespace mysql = sqlpp::mysql;
 
-
-bool operator == (const mss::User& lhs, const mss::User& rhs)
+static bool operator==(const mss::Domain& lhs, const mss::Domain& rhs)
 {
-    return std::make_tuple(lhs.user_id(), lhs.username(), lhs.domain_id(), lhs.status()) == std::make_tuple(rhs.user_id(), rhs.username(), rhs.domain_id(), rhs.status());
+    return std::make_tuple(lhs.domain_id(), lhs.hostname(), lhs.external()) ==
+           std::make_tuple(rhs.domain_id(), rhs.hostname(), rhs.external());
 }
 
-bool operator == (const mss::Chat& lhs, const mss::Chat& rhs)
+static bool operator==(const mss::User& lhs, const mss::User& rhs)
 {
-    return std::make_tuple(lhs.chat_id(), lhs.domain_id(), lhs.chatname()) == std::make_tuple(rhs.chat_id(), rhs.domain_id(), rhs.chatname());
+    return std::make_tuple(lhs.user_id(), lhs.username(), lhs.domain_id(), lhs.status()) ==
+           std::make_tuple(rhs.user_id(), rhs.username(), rhs.domain_id(), rhs.status());
 }
 
-bool operator == (const mss::Message& lhs, const mss::Message& rhs)
+static bool operator==(const mss::Chat& lhs, const mss::Chat& rhs)
 {
-    return std::make_tuple(lhs.message_id(), lhs.chat_id(), lhs.domain_id_chat(),
-                           lhs.user_id(), lhs.domain_id_user(), lhs.text(), std::chrono::system_clock::to_time_t(lhs.timestamp()), lhs.status()) ==
-           std::make_tuple(rhs.message_id(), rhs.chat_id(), rhs.domain_id_chat(),
-                           rhs.user_id(), rhs.domain_id_user(), rhs.text(), std::chrono::system_clock::to_time_t(rhs.timestamp()), rhs.status());
+    return std::make_tuple(lhs.chat_id(), lhs.domain_id(), lhs.chatname()) ==
+           std::make_tuple(rhs.chat_id(), rhs.domain_id(), rhs.chatname());
 }
 
-template <class T>
-bool find(const std::vector<T>& vec, const T& obj)
+static std::ostream& operator<<(std::ostream& o, const mc::Message::Status status)
 {
-    for (const auto& a: vec)
+    switch (status)
     {
-        if (a == obj)
-            return true;
+    case mc::Message::Status::FAIL:
+        o << "FAIL";
+        break;
+    case mc::Message::Status::SENT:
+        o << "SENT";
+        break;
+    case mc::Message::Status::RECEIVED:
+        o << "RECEIVED";
+        break;
+    case mc::Message::Status::SEEN:
+        o << "SEEN";
+        break;
     }
-    return false;
+    return o;
 }
 
-TEST_CASE( "Test storage service", "[storage]" )
+static std::ostream& operator<<(std::ostream& o, const mc::Message& message)
+{
+    o << "Message { "
+      << "message_id: " << message.message_id() << ", "
+      << "chat_id: " << message.chat_id() << ", "
+      << "domain_id_chat: " << message.domain_id_chat() << ", "
+      << "user_id: " << message.user_id() << ", "
+      << "domain_id_user: " << message.domain_id_user() << ", "
+      << "text: \"" << message.text() << "\", "
+      << "status: " << message.status() << " } ";
+    return o;
+}
+
+static bool operator==(const mss::Message& lhs, const mss::Message& rhs)
+{
+    std::cerr << "\nM1: " << lhs << std::endl << "M2: " << rhs << std::endl << std::endl;
+    return std::make_tuple(lhs.message_id(), lhs.chat_id(), lhs.domain_id_chat(), lhs.text(),
+                           lhs.user_id(), lhs.domain_id_user(), lhs.status(), lhs.timestamp()) ==
+           std::make_tuple(rhs.message_id(), rhs.chat_id(), rhs.domain_id_chat(), rhs.text(),
+                           rhs.user_id(), rhs.domain_id_user(), rhs.status(), rhs.timestamp());
+}
+
+TEST_CASE("Test storage service", "[storage]")
 {
     mysql::connection db(mss::config_db());
     db.execute(R"(SET autocommit=0)");
 
-    SECTION( "Test domains" )
+    SECTION("Test domains")
     {
         mss::Domain domain1(db, "Paul server", false);
         mss::Domain domain2(db, "Blaze server", false);
@@ -52,18 +84,14 @@ TEST_CASE( "Test storage service", "[storage]" )
         {
             REQUIRE_NOTHROW(mss::check_if_domain_exists(db, domain1.domain_id()));
             mss::Domain found_domain1(db, domain1.hostname());
-            REQUIRE(domain1.domain_id() == found_domain1.domain_id());
-            REQUIRE(domain1.hostname() == found_domain1.hostname());
-            REQUIRE(domain1.external() == found_domain1.external());
+            REQUIRE(domain1 == found_domain1);
 
             REQUIRE_NOTHROW(mss::check_if_domain_exists(db, domain2.domain_id()));
             mss::Domain found_domain2(db, domain2.hostname());
-            REQUIRE(domain2.domain_id() == found_domain2.domain_id());
-            REQUIRE(domain2.hostname() == found_domain2.hostname());
-            REQUIRE(domain2.external() == found_domain2.external());
+            REQUIRE(domain2 == found_domain2);
         }
 
-        SECTION( "Test users")
+        SECTION("Test users")
         {
             mss::User user1(db, "Anna", domain1.domain_id(), mc::User::Status::ONLINE);
             mss::User user2(db, "Erick", domain1.domain_id(), mc::User::Status::ONLINE);
@@ -71,16 +99,10 @@ TEST_CASE( "Test storage service", "[storage]" )
             SECTION("Select inserted users")
             {
                 mss::User found_user1(db, user1.username(), user1.domain_id());
-                REQUIRE(user1.user_id() == found_user1.user_id());
-                REQUIRE(user1.username() == found_user1.username());
-                REQUIRE(user1.domain_id() == found_user1.domain_id());
-                REQUIRE(user1.status() == found_user1.status());
+                REQUIRE(user1 == found_user1);
 
                 mss::User found_user2(db, user2.username(), user2.domain_id());
-                REQUIRE(user2.user_id() == found_user2.user_id());
-                REQUIRE(user2.username() == found_user2.username());
-                REQUIRE(user2.domain_id() == found_user2.domain_id());
-                REQUIRE(user2.status() == found_user2.status());
+                REQUIRE(user2 == found_user2);
             }
 
             SECTION("Get online users")
@@ -98,7 +120,7 @@ TEST_CASE( "Test storage service", "[storage]" )
                 REQUIRE_FALSE(answer.empty());
             }
 
-            SECTION( "Test chats")
+            SECTION("Test chats")
             {
                 mss::Chat chat1(db, domain1.domain_id(), "secret_chat");
                 mss::Chat chat2(db, domain2.domain_id(), "On domain2");
@@ -107,9 +129,7 @@ TEST_CASE( "Test storage service", "[storage]" )
                 {
                     REQUIRE_NOTHROW(mss::check_if_chat_exists(db, chat1.chat_id(), chat1.domain_id()));
                     mss::Chat found_chat1(db, chat1.chat_id(), chat1.domain_id());
-                    REQUIRE(chat1.chat_id() == found_chat1.chat_id());
-                    REQUIRE(chat1.domain_id() == found_chat1.domain_id());
-                    REQUIRE(chat1.chatname() == found_chat1.chatname());
+                    REQUIRE(chat1 == found_chat1);
                 }
 
                 SECTION("Change chatname")
@@ -125,29 +145,22 @@ TEST_CASE( "Test storage service", "[storage]" )
                     found_chat1.add_user(user1);
                     found_chat1.add_user(user2);
                     std::vector<mss::User> users_in_chat = found_chat1.get_users();
-                    REQUIRE(find(users_in_chat, user1));
-                    REQUIRE(find(users_in_chat, user2));
+                    REQUIRE(std::find_if(users_in_chat.begin(), users_in_chat.end(), [&user1](const mss::User& u){ return u == user1; }) != users_in_chat.end());
+                    REQUIRE(std::find_if(users_in_chat.begin(), users_in_chat.end(), [&user2](const mss::User& u){ return u == user2; }) != users_in_chat.end());
                     REQUIRE(users_in_chat.size() == 2);
                 }
 
                 SECTION("Test messages")
                 {
-                    mss::Message message1(db, chat1.chat_id(), chat1.domain_id(), user2.user_id(), user2.domain_id(), ":D", std::chrono::system_clock::now(), mc::Message::Status::SENT);
-                    mss::Message message2(db, chat2.chat_id(), chat2.domain_id(), user2.user_id(), user2.domain_id(), "Lorem ipsum", std::chrono::system_clock::now(), mc::Message::Status::SENT);
+                    mss::Message message1(db, chat1.chat_id(), chat1.domain_id(), user2.user_id(), user2.domain_id(),
+                                          ":D", std::chrono::system_clock::now(), mc::Message::Status::SENT);
+                    mss::Message message2(db, chat2.chat_id(), chat2.domain_id(), user2.user_id(), user2.domain_id(),
+                                          "Lorem ipsum", std::chrono::system_clock::now(), mc::Message::Status::SENT);
 
                     SECTION("Select inserted messages")
                     {
                         mss::Message found_message1(db, message1.message_id(), message1.chat_id(), message1.domain_id_chat());
-                        REQUIRE(message1.message_id() == found_message1.message_id());
-                        REQUIRE(message1.chat_id() == found_message1.chat_id());
-                        REQUIRE(message1.domain_id_chat() == found_message1.domain_id_chat());
-                        REQUIRE(message1.user_id() == found_message1.user_id());
-                        REQUIRE(message1.domain_id_user() == found_message1.domain_id_user());
-                        REQUIRE(message1.text() == found_message1.text());
-                        std::time_t time1 = std::chrono::system_clock::to_time_t(message1.timestamp());
-                        std::time_t time2 = std::chrono::system_clock::to_time_t(found_message1.timestamp());
-                        REQUIRE(time1 == time2);
-                        REQUIRE(message1.status() == found_message1.status());
+                        REQUIRE(message1 == found_message1);
                     }
 
                     SECTION("Change status")
@@ -164,14 +177,17 @@ TEST_CASE( "Test storage service", "[storage]" )
                         REQUIRE(found_message1.text() == "((((((");
                     }
 
-                    SECTION( "Test get_messages() for Chat")
+                    SECTION("Test get_messages() for Chat")
                     {
                         mss::Chat found_chat1(db, chat1.chat_id(), chat1.domain_id());
-                        mss::Message message1(db, chat1.chat_id(), chat1.domain_id(), user2.user_id(), user2.domain_id(), ":D", std::chrono::system_clock::now(), mc::Message::Status::SENT);
-                        mss::Message message2(db, chat1.chat_id(), chat1.domain_id(), user2.user_id(), user2.domain_id(), "Lorem ipsum", std::chrono::system_clock::now(), mc::Message::Status::SENT);
+                        mss::Message message1(db, chat1.chat_id(), chat1.domain_id(), user2.user_id(), user2.domain_id(),
+                                              ":D", std::chrono::system_clock::now(), mc::Message::Status::SENT);
+                        mss::Message message2(db, chat1.chat_id(), chat1.domain_id(), user2.user_id(), user2.domain_id(),
+                                              "Lorem ipsum", std::chrono::system_clock::now(), mc::Message::Status::SENT);
                         std::vector<mss::Message> messages_in_chat = found_chat1.get_messages();
-                        REQUIRE(find(messages_in_chat, message1));
-                        REQUIRE(find(messages_in_chat, message2));
+                        REQUIRE(std::find_if(messages_in_chat.begin(), messages_in_chat.end(), [&message1](const mss::Message& m){ return m == message1; }) != messages_in_chat.end());
+                        REQUIRE(std::find_if(messages_in_chat.begin(), messages_in_chat.end(), [&message2](const mss::Message& m){ return m == message2; }) != messages_in_chat.end());
+                        REQUIRE(messages_in_chat.size() == 2);
                     }
                 }
             }
