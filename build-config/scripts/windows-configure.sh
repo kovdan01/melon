5bfadd8d29ee69464874ffd84a6644291d2e2314
Daniil Kovalev <kovdan01@yandex.ru>
2021-05-06 21:05:36 +0300

    Use env variables instead of CLI arguments

--- build-config/scripts/windows-configure.sh
+++ build-config/scripts/windows-configure.sh
@@ -2,18 +2,18 @@ $CMAKE_TOOL_BINARY_PATH\\cmake \
     -G Ninja \
     -S . \
     -B build \
-    -D CMAKE_BUILD_TYPE=$1 \
-    -D CMAKE_RANLIB=/$2/bin/ranlib.exe \
+    -D CMAKE_BUILD_TYPE=$BUILD_TYPE \
+    -D CMAKE_RANLIB=/$MSYS_VARIANT/bin/ranlib.exe \
     -D CMAKE_VERBOSE_MAKEFILE=ON \
-    -D CMAKE_MAKE_PROGRAM=/$2/bin/ninja.exe \
-    -D CMAKE_C_COMPILER=/$2/bin/gcc.exe \
-    -D CMAKE_CXX_COMPILER=/$2/bin/g++.exe \
-    -D CMAKE_AR=/$2/bin/ar.exe \
-    -D CMAKE_LINKER=/$2/bin/ld.exe \
-    -D CMAKE_DLLTOOL=/$2/bin/dlltool.exe \
-    -D CMAKE_NM=/$2/bin/nm.exe \
+    -D CMAKE_MAKE_PROGRAM=/$MSYS_VARIANT/bin/ninja.exe \
+    -D CMAKE_C_COMPILER=/$MSYS_VARIANT/bin/gcc.exe \
+    -D CMAKE_CXX_COMPILER=/$MSYS_VARIANT/bin/g++.exe \
+    -D CMAKE_AR=/$MSYS_VARIANT/bin/ar.exe \
+    -D CMAKE_LINKER=/$MSYS_VARIANT/bin/ld.exe \
+    -D CMAKE_DLLTOOL=/$MSYS_VARIANT/bin/dlltool.exe \
+    -D CMAKE_NM=/$MSYS_VARIANT/bin/nm.exe \
     -D CMAKE_PREFIX_PATH=\
-    "/$2;\
+    "/$MSYS_VARIANT;\
     $ROOT_PATH/$BOOST_INSTALL_PATH;\
     $ROOT_PATH/$SQLPP11_INSTALL_PATH;\
     $ROOT_PATH/$CATCH2_INSTALL_PATH;\
